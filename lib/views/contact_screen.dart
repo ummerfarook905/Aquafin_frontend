9c2a029fd8b38d958a9b4ee44268c9995075e6d3
ummerfarook905 <ummerfarook905@gmail.com>
2025-09-11 00:33:34 +0530

    editing contact page

--- lib/views/contact_screen.dart
+++ lib/views/contact_screen.dart
@@ -15,66 +15,71 @@ class ContactScreen extends StatelessWidget {
       body: Obx(() {
         final contact = controller.contact.value;
 
-        return Padding(
-          padding: const EdgeInsets.all(20.0),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              const SizedBox(height: 40),
-              
-              const Text(
-                "Contact us",
-                style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 22,
-                  fontWeight: FontWeight.bold,
-                  fontFamily: "Roboto"
+        return Center(
+          child: Padding(
+            padding: const EdgeInsets.all(20.0),
+            child: Column(
+              children: [
+                const SizedBox(height: 40),
+                
+                const Text(
+                  "Contact us",
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 22,
+                    fontWeight: FontWeight.bold,
+                    fontFamily: "Roboto"
+                  ),
                 ),
-              ),
-
-              const SizedBox(height: 40),
-
-              const Text(
-                "Phone number",
-                style: TextStyle(color: Colors.white, fontSize: 16),
-              ),
-              const SizedBox(height: 8),
-              Text(
-                contact.phone,
-                style: const TextStyle(
-                  color: Colors.white,
-                  fontSize: 18,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-              const SizedBox(height: 20),
-
-              const Text(
-                "Email",
-                style: TextStyle(color: Colors.white,fontSize: 16),
-              ),
-              const SizedBox(height: 8),
-              Text(
-                contact.email,
-                textAlign: TextAlign.center,
-                style: const TextStyle(
-                  color: Colors.white,
-                  fontSize: 16,
-                  fontWeight: FontWeight.bold),
-              ),
-              const Spacer(),
-
-              TextButton(
-                onPressed: () {
-                  Get.offAll(() => HomeScreen());
-                }, 
-                child: const Text(
-                  "Back",
+          
+                const SizedBox(height: 40),
+          
+                const Text(
+                  "Phone number",
                   style: TextStyle(color: Colors.white, fontSize: 16),
-                ))
-            ],
-          ),
-          );
+                ),
+                const SizedBox(height: 8),
+                Text(
+                  contact.phone,
+                  style: const TextStyle(
+                    color: Colors.white,
+                    fontSize: 18,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                const SizedBox(height: 20),
+          
+                const Text(
+                  "Email",
+                  style: TextStyle(color: Colors.white,fontSize: 16),
+                ),
+                const SizedBox(height: 8),
+                Text(
+                  contact.email,
+                  textAlign: TextAlign.center,
+                  style: const TextStyle(
+                    color: Colors.white,
+                    fontSize: 16,
+                    fontWeight: FontWeight.bold),
+                ),
+                const Spacer(),
+          
+                Align(
+                  alignment: Alignment.centerLeft, // ⬅ moves to left
+                  child: TextButton(
+                    onPressed: () {
+                      Get.offAll(() => HomeScreen());
+                    },
+                    child: const Text(
+                      "Back",
+                      style: TextStyle(color: Colors.white, fontSize: 16),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            ),
+        );
       }),
     );
   }
